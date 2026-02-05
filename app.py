ef15d99250aea2b569126a5cabedef1e5c863def
Taibi farouk <120414859+TFDAdonis@users.noreply.github.com>
2026-02-05 15:09:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2336,7 +2336,7 @@ else:
                                     nir.add(red).add(0.5)
                                 ).rename('SAVI')
                                 indices.append(savi)
-                                 if 'NDWI' in params['selected_indices']:
+                          if 'NDWI' in params['selected_indices']:
                             ndwi = green.subtract(nir).divide(green.add(nir)).rename('NDWI')
                             indices.append(ndwi)
                         
